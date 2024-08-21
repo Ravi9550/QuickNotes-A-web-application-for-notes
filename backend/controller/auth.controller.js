1657dddee4fa4d91a1de33af770c32cde25bcf71
ravi gupta <ravigupta13102004@gmail.com>
2024-08-21 21:44:51 +0530

    first commit

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -37,19 +37,19 @@ export const signin = async (req, res, next) => {
 
   try {
     const validUser = await User.findOne({ email })
-
+    console.log(validUser)
     if (!validUser) {
       return next(errorHandler(404, "User not found"))
     }
 
     const validPassword = bcryptjs.compareSync(password, validUser.password)
-
+    console.log(validPassword)
     if (!validPassword) {
       return next(errorHandler(401, "Wrong Credentials"))
     }
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
-
+    
     const { password: pass, ...rest } = validUser._doc
 
     res.cookie("access_token", token, { httpOnly: true }).status(200).json({
